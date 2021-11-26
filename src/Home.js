aab8f96631c2e495dc403bd452409131984c4bce
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-26 14:41:43 -0300

    feat: Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    
+    const handleClick = (e) => {
+        console.log('hello ninjas',e)
+    }
+
+    const handleClickParam = (name) =>{
+        console.log(`Hello ${name}`);
+    }
+
     return ( 
        <div className="home">
            <h2>Homepage</h2>
+           <button onClick={handleClick}>Click me</button>
+           <button onClick={()=>handleClickParam('Gabriel')}>Click me again</button>
        </div> 
      );
 }
